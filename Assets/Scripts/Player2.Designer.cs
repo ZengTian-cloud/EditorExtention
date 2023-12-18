430b727222357009dd5b0f40977866050892f273
Tian Zhen <506748308@qq.com>
2023-12-18 23:50:41 +0800

    123

---
+++ Assets/Scripts/Player2.Designer.cs
@@ -0,0 +1,13 @@
+// Generate Id:b9e82d85-23aa-48c3-8c69-0f2195d8f0ed
+using UnityEngine;
+
+namespace Sikiedu
+{
+	public partial class Player2
+	{
+		public Transform Weapon;
+		public Transform Animation;
+		public Transform Physics;
+
+	}
+}
---
+++ Assets/Scripts/Player2.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using EditorExtension;
+
+namespace Sikiedu
+{
+	public partial class Player2 : CodeGenerateInfo
+	{
+		void Start()
+		{
+			// Code Here
+		}
+	}
+}
